5b3b3253229aa3d6ca627e766189ce00e0232103
DavidChen108061240 <72589020+DavidChen108061240@users.noreply.github.com>
2020-11-16 13:45:04 +0800

    Add files via upload

--- project_2.cpp
+++ project_2.cpp
@@ -1,104 +1,139 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stack>
+#include <queue>
 #include <fstream>
 #include <iomanip>
 #include <cstring>
 using namespace std;
-#define N 1000
 
+
+   
 class Pos {
 public:    
     int x, y;
+    int visited;
 };
 class Ground {
 public:
     int row, col, life;                                 // row col power
-    char ground[N][N];                                  // whole ground
+    char **ground;                                      // whole ground
+
     Pos start;                                          // start point, also recharge place
+    Pos visited_node;
     
 };
 
-Pos clean (Ground gnd, Pos cur_pos) {
+Pos clean (Ground &gnd, Pos cur_pos) {
     int x, y;
     Pos next_pos;
-    int donothing = 0;
-    next_pos.x = -1;
-    next_pos.y = -1;
+
+    next_pos.x = 0;
+    next_pos.y = 0;
+    next_pos.visited = -1;
     x = cur_pos.x;
     y = cur_pos.y;
     
-    if(gnd.ground[x][y + 1] == '0' && y != gnd.col - 1)  {           // east, need to be cleaned
+    if(gnd.ground[x][y + 1] == '0' &&  y != gnd.col - 1)  {           // east, need to be cleaned
         next_pos.x = x;
         next_pos.y = y + 1;
+        next_pos.visited = 1;
     } else if (gnd.ground[x + 1][y] == '0' && x != gnd.row - 1) {    // south, need to be clean
         next_pos.x = x + 1;
         next_pos.y = y;
+        next_pos.visited = 1;
     } else if (gnd.ground[x][y - 1] == '0' && y != 0) {              // west, need to be clean
         next_pos.x = x;
         next_pos.y = y - 1;
+        next_pos.visited = 1;
     } else if (gnd.ground[x - 1][y] == '0' && x != 0) {              // north, need to be clean
         next_pos.x = x - 1;
         next_pos.y = y;
-    } else {
-        donothing = 1;
-    }
+        next_pos.visited = 1;
+    } 
     return next_pos;
 } 
 
-void Ground_Path(Ground gnd) {
+void Ground_Path(Ground &gnd) {
     Pos cur_pos, next_pos;                                // current position, next_position
     stack<Pos> path_stack;                                // path
+    stack<Pos> tmp_stack;
+    queue<Pos> whole_path_queue;                          // Whole path
     int decreasing_life = gnd.life;                       // renew life
     cur_pos = gnd.start;
+    int SUM_STEP = 0;
+    int NO_battery = 0;
+    int RENEW_STEP = 0;
+    int many_stack = 0;
+
     do {
-        next_pos = clean(gnd, cur_pos);
-        decreasing_life--;
-        if (cur_pos.x == gnd.start.x && cur_pos.y == gnd.start.y) {
+        /*if(!NO_battery) {
+            while (!path_stack.empty()) {               // pop the best road, back to R pt to recharge
+                cur_pos = path_stack.top();
+                   
+                whole_path_queue.push(cur_pos);
+                tmp_stack.push(cur_pos);
+                path_stack.pop();
+                SUM_STEP++;
+            }
+            many_stack = 0;
+            decreasing_life = gnd.life;
+            NO_battery = 0;
+            
+            while (!tmp_stack.empty()) {
+                cur_pos = tmp_stack.top();
+
+                whole_path_queue.push(cur_pos);
+                path_stack.push(cur_pos);
+                many_stack++;
+                decreasing_life--;
+                tmp_stack.pop();
+                SUM_STEP++;
+            }
+                                                            // two while loop end, cur_pos is where the last time need to charge location
+        }*/
+            next_pos = clean(gnd, cur_pos);
+                
+        if (next_pos.visited != -1) {
             path_stack.push(cur_pos);
-            gnd.ground[cur_pos.x][cur_pos.y] = 'R';
-            decreasing_life = gnd.life; 
-        } else if (next_pos.x != -1) {
-            path_stack.push(cur_pos);
-            gnd.ground[cur_pos.x][cur_pos.y] = 'A';         // have already gone 1 time
+            whole_path_queue.push(cur_pos);
+            gnd.ground[cur_pos.x][cur_pos.y] = 'A';         // have already gone
+                
             cur_pos = next_pos;
+            decreasing_life--;
+            RENEW_STEP++;                                   // move forward, step+1, life-1
+            SUM_STEP++;                                     // sum step
+            many_stack++;
+            /*if((decreasing_life - many_stack) <= 1) {
+                NO_battery = 1;                             /////// go back R
+            }   */     
         } else {                                            // four way can not go 
             gnd.ground[cur_pos.x][cur_pos.y] = 'E';         // this is the end of this way
             cur_pos = path_stack.top();
+            whole_path_queue.push(cur_pos);                       // pop element, move back, push into queue
             path_stack.pop();
-            decreasing_life++;
+            decreasing_life--;
+
+            RENEW_STEP++;                                   // move back, step+1, life-1
+            SUM_STEP++;
+            many_stack--;
+            /*if((decreasing_life - many_stack) <= 1) {
+                NO_battery = 1;      ///////  go back R
+            }*/
         } 
     } while (!path_stack.empty());
+        
+    cout << "whole step is " << SUM_STEP << endl;
 
     if (path_stack.empty()) {
         cout << "dead road "<< endl;
     } else {
-        stack<Pos> tmp_stack;
-        int order = 0;
-        while (!path_stack.empty()) {
-            cur_pos = path_stack.top();
-            path_stack.pop();
-            tmp_stack.push(cur_pos);
+        while (!whole_path_queue.empty()) {
+            cur_pos = whole_path_queue.front();
+            whole_path_queue.pop();
+            cout << setw(4) << cur_pos.x << cur_pos.y << endl;
         }
-        while (!tmp_stack.empty()) {
-            order++;
-            cur_pos = tmp_stack.top();
-            tmp_stack.pop();
-            gnd.ground[cur_pos.x][cur_pos.y] = order;
-        }
-        cout << endl;
-
-        for (int i = 0; i < gnd.row; i++) {
-            for (int j = 0; j < gnd.col; j++) {
-                if(gnd.ground[i][j] > 0) {
-                    cout << setw(2) << gnd.ground[i][j] << " ";
-                } else {
-                    cout << setw(2) << " ! ";
-                }
-            }
-            cout << endl;
-        }
-    }
+    }      
 }
 
 int main (int argc, char **argv) {
@@ -120,11 +155,25 @@ int main (int argc, char **argv) {
         return -1;
     }
     file_in >> G.row >> G.col >> G.life;
-    
-    cout << G.row << " " << G.col << " " << G.life << endl;
 
+    G.ground = new char* [G.row];
+    for (int i = 0; i < G.row; ++i) {
+            G.ground[i] = new char [G.col];                                      // bool has value 1 or 0
+    }
+    for (int i = 0; i < G.row; ++i) {                                         // set all value 0
+        for (int j = 0; j < G.col; ++j) {
+            file_in >> G.ground[i][j] ;
+            cout << G.ground[i][j] << " ";
+            if (G.ground[i][j] == 'R') {
+                G.start.x = i;
+                G.start.y = j;
+            }
+        }   
+        cout << endl;         
+    }
+    Ground_Path(G);
+/*
     for ( int i = 0; i < G.row ; i++) {
-        
         for (int j = 0; (j < G.col) ; j++ ) { 
             file_in >> G.ground[i][j];
                   
@@ -133,13 +182,12 @@ int main (int argc, char **argv) {
                 G.start.x = i;
                 G.start.y = j;
             }
-      
         }
         cout << endl;
-        
     }
+*/
     file_in.close();
-    // cout << G.start.x << " " << G.start.y << endl;
+    cout << G.start.x << " " << G.start.y << endl;
 
 
     return 0;
